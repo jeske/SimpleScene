7af9ae1d6cb3afbf8ee84a18d03ca35b376a2c34
David Jeske <davidj@gmail.com>
2014-07-30 22:18:53 -0700

    size the text image before creating it

--- SimpleScene/Objects/SSObject2DSurface_AGGText.cs
+++ SimpleScene/Objects/SSObject2DSurface_AGGText.cs
@@ -30,12 +30,14 @@ namespace SimpleScene {
 
         public override ImageBuffer RepaintAGG(out Vector2 gdiSize) {
             // figure out the size of the label			
-			
-			ImageBuffer bitmap = new ImageBuffer(200,50,32, new BlenderBGRA());
-			var gc = bitmap.NewGraphics2D();			
-			gc.Line(0,10,200,50, new MatterHackers.Agg.RGBA_Bytes(200,0,0));	
-			gc.Circle(20,20,10,new MatterHackers.Agg.RGBA_Bytes(0,200,0));		
-			gc.DrawString(_label,10,10,14,color: new MatterHackers.Agg.RGBA_Bytes(255,255,255));		
+			var tfp = new TypeFacePrinter(_label);
+			var size = tfp.GetSize(_label);			
+			ImageBuffer bitmap = new ImageBuffer((int)size.x,(int)size.y,32, new BlenderBGRA());
+			var gc = bitmap.NewGraphics2D();	
+
+			gc.Line(0,0,size.x,0, new MatterHackers.Agg.RGBA_Bytes(200,0,0));	
+			gc.Circle(20,20,10,new MatterHackers.Agg.RGBA_Bytes(0,200,0));			
+			gc.DrawString(_label,0,0,14,color: new MatterHackers.Agg.RGBA_Bytes(255,255,255));		
 			
 			gdiSize = new Vector2(bitmap.Width,bitmap.Height);
 
