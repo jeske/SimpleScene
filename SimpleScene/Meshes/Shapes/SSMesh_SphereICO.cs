d1cfb9ecab3ad54c5e5d87a3a717e8830824716a
David Jeske <davidj@gmail.com>
2014-08-12 14:26:15 -0700

    properly generate "wrapping" icosphere UV coordinates

--- SimpleScene/Meshes/Shapes/SSMesh_SphereICO.cs
+++ SimpleScene/Meshes/Shapes/SSMesh_SphereICO.cs
@@ -42,12 +42,12 @@ namespace SimpleScene
 			// using Y-up coordinates
 			
 			// compute azimuth (x texture coord)
-			float longitude = (float) Math.Atan2(vpn.X,vpn.Z);  // azimuth [-PI  to PI]   			
-			u = (float) (( longitude / (2.0 * Math.PI) ) + 0.5);        // [0 to 1]
+			float longitude = (float) Math.Atan2(vpn.X,vpn.Z);       // azimuth [-PI  to PI]   			
+			u = (float) (( longitude / (2.0 * Math.PI) ) + 0.5);     // [0 to 1]
 
 			// compute altitude (y texture coord)
-			float latitude = (float) Math.Acos(vpn.Y);          // altitude [0 to PI]
-			v = (float) (latitude / Math.PI);                   // [0 to 1]
+			float latitude = (float) Math.Acos(vpn.Y);               // altitude [0 to PI]
+			v = (float) (latitude / Math.PI);                        // [0 to 1]
 
 			// v = (float) ( Math.Log((1.0 + Math.Sin(latitude))/(1.0 - Math.Sin(latitude))) / (4.0 * Math.PI) );
 		}
@@ -55,35 +55,81 @@ namespace SimpleScene
 		private void _Create(int divisions) {
 			var icoSphereCreator = new IcoSphereCreator();
 			geom = icoSphereCreator.Create(divisions);
+			var positions = geom.Positions.ToArray();
 
-			num_vertices = geom.Positions.Count;
-			num_indicies = geom.MeshIndicies.Count;
-			var geom_verts = geom.Positions.ToArray();
-			var geom_indicies = geom.MeshIndicies.ToArray();
+			var vertexSoup = new Util3d.VertexSoup<SSVertex_PosNormDiffTex1>();
+			var indexList = new List<UInt16>();
 
-			SSVertex_PosNormDiffTex1[] vertices = new SSVertex_PosNormDiffTex1[num_vertices];
-			UInt16[] indicies = new UInt16[num_indicies];
+			// we have to process each face in the IcoSphere, so we can
+			// properly "wrap" the texture-coordinates that fall across the left/right border
+			foreach (TriangleIndices face in geom.Faces) {
+				var vp1 = positions[face.v1];
+				var vp2 = positions[face.v2];
+				var vp3 = positions[face.v3];
 
-			// populate vertex array
-			for(int vi = 0; vi < num_vertices ; vi++ ) {
-				vertices[vi].Position = geom_verts[vi];
-				vertices[vi].Normal = geom_verts[vi].Normalized(); // surface normal is the unit vector
-				vertices[vi].DiffuseColor = System.Drawing.Color.White.ToArgb();
+				var normal1 = vp1.Normalized();
+				var normal2 = vp2.Normalized();
+				var normal3 = vp3.Normalized();
 
-				_computeEquirectangularUVForSpherePoint(
-					vertices[vi].Normal,
-					out vertices[vi].Tu,
-					out vertices[vi].Tv);
+				float s1,s2,s3,t1,t2,t3;
+
+			    _computeEquirectangularUVForSpherePoint(normal1,out s1, out t1);
+				_computeEquirectangularUVForSpherePoint(normal2,out s2, out t2);
+				_computeEquirectangularUVForSpherePoint(normal3,out s3, out t3);
+
+				// configure verticies
+
+				var v1 = new SSVertex_PosNormDiffTex1();
+				v1.Position = vp1;
+				v1.Normal = normal1;
+				v1.Tu = s1;
+				v1.Tv = t1;
+
+				var v2 = new SSVertex_PosNormDiffTex1();
+				v2.Position = vp2;
+				v2.Normal = normal2;
+				v2.Tu = s2;
+				v2.Tv = t2;
+
+				var v3 = new SSVertex_PosNormDiffTex1();
+				v3.Position = vp3;
+				v3.Normal = normal3;
+				v3.Tu = s3;
+				v3.Tv = t3;
+
+				// if a triangle spans the left/right seam where U transitions from 1 to -1
+				bool v1_left = vp1.X < 0.0f;
+				bool v2_left = vp2.X < 0.0f;
+				bool v3_left = vp3.X < 0.0f;
+				if (vp1.Z < 0.0f && vp2.Z < 0.0f && vp3.Z < 0.0f &&
+                    ((v2_left != v1_left) || (v3_left != v1_left))) {
+					// we need to "wrap" texture coordinates
+					if (v1.Tu < 0.5f) { v1.Tu += 1.0f; }
+					if (v2.Tu < 0.5f) { v2.Tu += 1.0f; }
+					if (v3.Tu < 0.5f) { v3.Tu += 1.0f; }
+				}
+
+				// add configured verticies to mesh..
+
+				UInt16 idx1 = vertexSoup.digestVertex(ref v1);
+				UInt16 idx2 = vertexSoup.digestVertex(ref v2);
+				UInt16 idx3 = vertexSoup.digestVertex(ref v3);
+
+				indexList.Add(idx1);
+				indexList.Add(idx2);
+				indexList.Add(idx3);
 			}
-			// populate index array
-			for(int ii=0; ii < num_indicies ; ii++) {
-				indicies[ii] = (UInt16) geom_indicies[ii];
-			}
+
+			var vertexArr = vertexSoup.verticies.ToArray();
+			var idxArr = indexList.ToArray();
+
+			num_vertices = vertexArr.Length;
+			num_indicies = idxArr.Length;
 
 			// upload to GL
 
-			vbo = new SSVertexBuffer<SSVertex_PosNormDiffTex1>(vertices);
-			ibo = new SSIndexBuffer<UInt16> (indicies, sizeof(UInt16));	
+			vbo = new SSVertexBuffer<SSVertex_PosNormDiffTex1>(vertexArr);
+			ibo = new SSIndexBuffer<UInt16> (idxArr, sizeof(UInt16));	
 		}
 
 
@@ -96,13 +142,12 @@ namespace SimpleScene
 			} else {
 				GL.BindTexture(TextureTarget.Texture2D, 0);
 			}
-			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToEdge);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToEdge);
 
 			vbo.bind (this.shaderPgm);
 			ibo.bind ();
 
-			//GL.DrawArrays (PrimitiveType.Triangles, 0, 6);
 			GL.DrawElements (PrimitiveType.Triangles, num_indicies, DrawElementsType.UnsignedShort, IntPtr.Zero);
 			ibo.unbind ();
 			vbo.unbind ();	
