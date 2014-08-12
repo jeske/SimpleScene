962ef5f99ca31088a1a37abe0c74a84475d53ac3
David Jeske <davidj@gmail.com>
2014-08-12 12:11:28 -0700

    WIP: equirectangular UV computation (broken)

--- SimpleScene/Meshes/Shapes/SSMesh_SphereICO.cs
+++ SimpleScene/Meshes/Shapes/SSMesh_SphereICO.cs
@@ -22,10 +22,30 @@ namespace SimpleScene
 		int num_indicies;
 		int num_vertices;
 
-		public SSMesh_SphereICO (int divisions, SSShaderProgram shaderPgm)
+		SSTexture texture;
+
+		public SSMesh_SphereICO (int divisions, SSShaderProgram shaderPgm, SSTexture texture)
 		{
 			this.shaderPgm = shaderPgm;
+			this.texture = texture;
+
 			this._Create(divisions);
+		}
+
+		private void _computeEquirectangularUVForSpherePoint(Vector3 p, out float u, out float v) {
+			// http://paulbourke.net/geometry/transformationprojection/
+
+			// this is the unit surface normal (and surface point)
+			Vector3 vpn = p.Normalized();
+
+			// compute latitute and longitute values from the vector to the normalized sphere surface position
+			// using Y-up coordinates
+			
+			float latitude = (float) Math.Atan2(vpn.Y, Math.Sqrt( vpn.X * vpn.X + vpn.Z * vpn.Z) );
+			float longitude = (float) Math.Atan2(vpn.X,vpn.Z);
+
+			u = (float) ( longitude / Math.PI ) ;
+			v = (float) ( Math.Log((1.0 + Math.Sin(latitude))/(1.0 - Math.Sin(latitude))) / (4.0 * Math.PI) );
 		}
 
 		private void _Create(int divisions) {
@@ -33,12 +53,30 @@ namespace SimpleScene
 			geom = icoSphereCreator.Create(divisions);
 
 			num_vertices = geom.Positions.Count;
-			num_indicies = geom.MeshIndicies.Count * 3;
+			num_indicies = geom.MeshIndicies.Count;
+			var geom_verts = geom.Positions.ToArray();
+			var geom_indicies = geom.MeshIndicies.ToArray();
 
 			SSVertex_PosNormDiffTex1[] vertices = new SSVertex_PosNormDiffTex1[num_vertices];
 			UInt16[] indicies = new UInt16[num_indicies];
 
-			// populate the arrays
+			// populate vertex array
+			for(int vi = 0; vi < num_vertices ; vi++ ) {
+				vertices[vi].Position = geom_verts[vi];
+				vertices[vi].Normal = geom_verts[vi].Normalized(); // surface normal is the unit vector
+				vertices[vi].DiffuseColor = System.Drawing.Color.White.ToArgb();
+
+				_computeEquirectangularUVForSpherePoint(
+					vertices[vi].Normal,
+					out vertices[vi].Tu,
+					out vertices[vi].Tv);
+			}
+			// populate index array
+			for(int ii=0; ii < num_indicies ; ii++) {
+				indicies[ii] = (UInt16) geom_indicies[ii];
+			}
+
+			// upload to GL
 
 			vbo = new SSVertexBuffer<SSVertex_PosNormDiffTex1>(vertices);
 			ibo = new SSIndexBuffer<UInt16> (indicies, sizeof(UInt16));	
@@ -46,6 +84,17 @@ namespace SimpleScene
 
 
 		public override void RenderMesh(ref SSRenderConfig renderConfig) {	
+			GL.UseProgram(shaderPgm.ProgramID);
+
+			GL.ActiveTexture(TextureUnit.Texture0);
+			if (texture != null) {
+				GL.BindTexture(TextureTarget.Texture2D, texture.TextureID);
+			} else {
+				GL.BindTexture(TextureTarget.Texture2D, 0);
+			}
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
 			vbo.bind (this.shaderPgm);
 			ibo.bind ();
 
