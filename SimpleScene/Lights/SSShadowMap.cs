b2d270c5e13937efa33a996fa3d6cdcd8b843324
HoneyHazard <zeromemory@gmail.com>
2014-10-25 06:21:24 -0700

    More working initialization of SSShadowMap

--- SimpleScene/Lights/SSShadowMap.cs
+++ SimpleScene/Lights/SSShadowMap.cs
@@ -19,25 +19,14 @@ namespace SimpleScene
 
         private int m_frameBufferID = 0;
         private int m_textureID = 0;
+        private bool m_isBound = false;
 
         public SSShadowMap()
         {
-            string version_string = GL.GetString(StringName.Version);
-            int major = int.Parse(version_string.Split(' ')[0]);
-            int minor = int.Parse(version_string.Split(' ')[1]);
-            Version version = new Version(major, minor);
-            Version versionRequired = new Version(3, 0);
-            if (version < versionRequired) {
-                throw new Exception("framebuffers not support by the GL backed used");
-            }
-
-			m_frameBufferID = GL.GenFramebuffer();
+            validateVersion();
             m_textureID = GL.GenTexture();
+            m_frameBufferID = GL.GenFramebuffer();
             bind();
-            GL.TexImage2D(TextureTarget.Texture2D, 0,
-                          PixelInternalFormat.DepthComponent16,
-                          c_texWidth, c_texHeight, 0,
-						  PixelFormat.DepthComponent, PixelType.Float, IntPtr.Zero);
             GL.TexParameter(TextureTarget.Texture2D, 
                             TextureParameterName.TextureMagFilter, 
                             (int)TextureMagFilter.Nearest);
@@ -50,38 +39,63 @@ namespace SimpleScene
             GL.TexParameter(TextureTarget.Texture2D, 
                             TextureParameterName.TextureWrapT, 
                             (int)TextureWrapMode.ClampToEdge);
+            GL.TexImage2D(TextureTarget.Texture2D, 0,
+                PixelInternalFormat.DepthComponent16,
+                c_texWidth, c_texHeight, 0,
+                PixelFormat.DepthComponent, PixelType.Float, IntPtr.Zero);
+
             GL.FramebufferTexture(FramebufferTarget.Framebuffer,
                                   FramebufferAttachment.DepthAttachment,
                                   m_textureID, 0);
 			assertFramebufferOK();
+            Unbind();
         }
 
         ~SSShadowMap() {
             Unbind ();
-            GL.DeleteFramebuffer(m_frameBufferID);
-            GL.DeleteTexture(m_textureID);
+            // DeleteData();
         }
 
-		public void PrepareForRender(ref SSRenderConfig renderConfig) {
+        public void DeleteData() {
+            // TODO: who/when calling this?
+            GL.DeleteTexture(m_textureID);
+            GL.DeleteFramebuffer(m_frameBufferID);
+        }
+
+        public void PrepareForRender(ref SSRenderConfig renderConfig) {
 			GL.DrawBuffer(DrawBufferMode.None);
-			// TODO: configure shadow map shader
+            //TODO: configure shadow map shader
 		}
 
 		public void Unbind() {
-			GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
-			GL.BindTexture(TextureTarget.Texture2D, 0);
+            if (m_isBound) {
+                GL.BindTexture(TextureTarget.Texture2D, 0);
+                GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+                m_isBound = false;
+            }
 		}
 
 		private void bind() {
+            m_isBound = true;
             GL.BindFramebuffer(FramebufferTarget.Framebuffer, m_frameBufferID);
             GL.BindTexture(TextureTarget.Texture2D, m_textureID);
         }
 
 		private void assertFramebufferOK() {
-			if (GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer) != FramebufferErrorCode.FramebufferComplete) {
-				throw new Exception("Frame buffer operation failed.");
+            var currCode = GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
+            if (currCode != FramebufferErrorCode.FramebufferComplete) {
+                throw new Exception("Frame buffer operation failed: " + currCode.ToString());
 			}
 		}
+
+        private void validateVersion() {
+            string version_string = GL.GetString(StringName.Version);
+            Version version = new Version(version_string[0], version_string[2]); // todo: improve
+            Version versionRequired = new Version(3, 0);
+            if (version < versionRequired) {
+                throw new Exception("framebuffers not supported by the GL backend used");
+            }
+        }
     }
 }
 
