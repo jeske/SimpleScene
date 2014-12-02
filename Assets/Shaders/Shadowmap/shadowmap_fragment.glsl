8d8cfd445770184437194dfd2db72e12ccaf1f3c
HoneyHazard <zeromemory@gmail.com>
2014-12-02 00:57:57 -0800

    pssm: More abstraction in the fragment shader

--- Assets/Shaders/Shadowmap/shadowmap_fragment.glsl
+++ Assets/Shaders/Shadowmap/shadowmap_fragment.glsl
@@ -3,19 +3,21 @@
 #version 120
 
 flat int f_shadowMapIndex = 0;
+const float SPLIT_WIDTH = 1024;
+const float SPLIT_HEIGHT = 1024;
 
 vec2 boundaries[4] = vec2[](
     vec2(0f, 0f),
-    vec2(1024f, 0f),
-    vec2(0f, 1024f),
-    vec2(1024f, 1024f)
+    vec2(SPLIT_WIDTH, 0f),
+    vec2(0f, SPLIT_HEIGHT),
+    vec2(SPLIT_WIDTH, SPLIT_HEIGHT)
 );
 
 void main()
 {
     //vec2 bmin = boundaries[f_shadowMapIndex];
     vec2 bmin = boundaries[0];
-    vec2 bmax = bmin + vec2(1024f);
+    vec2 bmax = bmin + vec2(SPLIT_WIDTH, SPLIT_HEIGHT);
     if (gl_FragCoord.x < bmin.x || gl_FragCoord.x >= bmax.x
      || gl_FragCoord.y < bmin.y || gl_FragCoord.y >= bmax.y) {
         discard;
