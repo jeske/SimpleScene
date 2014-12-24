fe429b790cdf54aacd391a2009a2be9046970c49
HoneyHazard <zeromemory@gmail.com>
2014-12-24 00:39:12 -0800

    Billboard trick works

--- SimpleScene/Objects/SSObjectBillboard.cs
+++ SimpleScene/Objects/SSObjectBillboard.cs
@@ -23,7 +23,7 @@ namespace SimpleScene
             if (Mesh != null) {
                 base.Render(ref renderConfig);
 
-                #if false
+                #if true
                 // override matrix setup to get rid of any rotation in view
                 // http://stackoverflow.com/questions/5467007/inverting-rotation-in-3d-to-make-an-object-always-face-the-camera/5487981#5487981
                 Matrix4 modelViewMat = this.worldMat * renderConfig.invCameraViewMat;
@@ -34,6 +34,7 @@ namespace SimpleScene
                     0f, scale.Y, 0f, trans.Y,
                     0f, 0f, scale.Z, trans.Z,
                     0f, 0f, 0f, 1f);
+                modelViewMat.Transpose();
                 GL.MatrixMode(MatrixMode.Modelview);
                 GL.LoadMatrix(ref modelViewMat);
                 #endif
