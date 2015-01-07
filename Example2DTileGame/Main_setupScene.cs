b1fd9a2b0cac7b26f4815685fffb8ef435935bc2
David Jeske <davidj@gmail.com>
2015-01-06 21:42:29 -0800

    scaffold for 2d tile game experiment

---
+++ Example2DTileGame/Main_setupScene.cs
@@ -0,0 +1,40 @@
+﻿// Copyright(C) David W. Jeske, 2013
+// Released to the public domain. Use, modify and relicense at will.
+
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
+using SimpleScene;
+
+namespace Example2DTileGame
+{
+	partial class Example2DTileGame : OpenTK.GameWindow
+	{
+
+		SSObjectGDISurface_Text fpsDisplay;
+		SSObjectGDISurface_Text wireframeDisplay;
+
+		public void setupScene() {
+		}
+
+		public void setupHUD() {
+			hudScene.ProjectionMatrix = Matrix4.Identity;
+
+			// HUD Triangle...
+			//SSObject triObj = new SSObjectTriangle ();
+			//hudScene.addObject (triObj);
+			//triObj.Pos = new Vector3 (50, 50, 0);
+			//triObj.Scale = new Vector3 (50.0f);
+
+			// HUD text....
+			fpsDisplay = new SSObjectGDISurface_Text ();
+			fpsDisplay.Label = "FPS: ...";
+			hudScene.AddObject (fpsDisplay);
+			fpsDisplay.Pos = new Vector3 (10f, 10f, 0f);
+			fpsDisplay.Scale = new Vector3 (1.0f);
+			
+		}
+	}
+}
