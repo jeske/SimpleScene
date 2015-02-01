bbab2afc13e6a2fec805bcef41ab58cc5733b0b4
HoneyHazard <zeromemory@gmail.com>
2015-01-31 16:03:42 -0800

    More work on the draft

--- SimpleScene/Meshes/ParticleSystem/ParticleSystem.cs
+++ SimpleScene/Meshes/ParticleSystem/ParticleSystem.cs
@@ -8,15 +8,42 @@ namespace SimpleScene
 {
     public class Particle
     {
+        public float Life = 0f;
         public Vector3 Pos = new Vector3(0f);
         public Vector3 Vel = new Vector3(0f);
+
+        virtual public void WriteParticle (ParticleSystem<Particle>ps, int idx)
+        {
+            ps.WriteLife(idx, Life);
+            ps.WritePosition(idx, Pos);
+            ps.WriteVelocity(idx, Vel);
+        }
+
+        virtual public void ReadParticle(ParticleSystem<Particle>ps, int idx)
+        {
+            Life = ps.ReadLife(idx);
+            Pos = ps.ReadPosition(idx);
+            Vel = ps.ReadVelocity(idx);
+        }
     }
 
-    public delegate void ParticleReceiver<P>(P newParticle) where P : Particle;
+    public delegate void ParticleReceiver<P>(P newParticle);
 
     public class ColoredParticle : Particle
     {
         public Color Color = Color.White;
+
+        public override void WriteParticle (ParticleSystem<Particle> ps, int idx)
+        {
+            base.WriteParticle(ps, idx);
+            ps.WriteColor(idx, Color);
+        }
+
+        public override void ReadParticle (ParticleSystem<Particle> ps, int idx)
+        {
+            base.ReadParticle(ps, idx);
+            Color = ps.ReadColor(idx);
+        }
     }
 
     public class ParticleWithMass : Particle
@@ -43,11 +70,71 @@ namespace SimpleScene
     public class ParticleSystem<P>
         where P : Particle
     {
-        protected List<P> m_particles = new List<P>();
+        protected List<ParticleEmitter<P>> m_emitters = new List<ParticleEmitter<P>> ();
         protected List<ParticleEffector<P>> m_effectors = new List<ParticleEffector<P>> ();
 
-        public ParticleSystem ()
+        protected readonly int m_capacity;
+
+        #region Required Particle Parameters
+        protected readonly float[] m_lives;  // unused particles will be hacked to not draw
+        protected readonly Vector3[] m_positions;
+        protected readonly Vector3[] m_velocities;
+        #endregion
+
+        #region Optional Particle Parameters
+        protected int[] m_colors = null;
+        protected float[] m_masses = null;
+        #endregion
+
+        internal void WriteLife(int idx, float life)
         {
+            m_lives [idx] = life;
+        }
+
+        internal float ReadLife(int idx) 
+        {
+            return m_lives [idx];
+        }
+
+        internal void WritePosition(int idx, Vector3 position)
+        {
+            m_positions [idx] = position;
+        }
+
+        internal Vector3 ReadPosition(int idx)
+        {
+            return m_positions[idx];
+        }
+
+        internal void WriteVelocity(int idx, Vector3 velocity)
+        {
+            m_velocities [idx] = velocity;
+        }
+
+        internal Vector3 ReadVelocity(int idx)
+        {
+            return m_velocities[idx];
+        }
+
+        internal void WriteColor(int idx, Color color)
+        {
+            if (m_colors == null) {
+                m_colors = new int[m_capacity];
+            }
+            m_colors [idx] = color.ToArgb();
+        }
+
+        internal Color ReadColor(int idx)
+        {
+            return Color.FromArgb (m_colors [idx]);
+        }
+
+        public ParticleSystem (int capacity)
+        {
+            m_capacity = capacity;
+            m_lives = new float[m_capacity];
+            m_positions = new Vector3[m_capacity];
+            m_velocities = new Vector3[m_capacity];
         }
 
 
