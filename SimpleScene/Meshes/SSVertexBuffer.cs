9576411b2abd257bf56330aee7f6ce1651c00eaf
HoneyHazard <zeromemory@gmail.com>
2015-02-02 15:33:54 -0800

    Moving some of SSVertexBuffer functionality to SSArrayBuffer so that it can be shared with attribute buffers

--- SimpleScene/Meshes/SSVertexBuffer.cs
+++ SimpleScene/Meshes/SSVertexBuffer.cs
@@ -10,42 +10,46 @@ namespace SimpleScene
 		void bindGLAttributes();
 	}
 
-	// http://www.opentk.com/doc/graphics/geometry/vertex-buffer-objects
-
     public interface ISSVertexBuffer
     {
         void DrawBind();
         void DrawUnbind();
     }
 
-    public class SSVertexBuffer<VB> : ISSVertexBuffer
-        where VB : struct, ISSVertexLayout {
-        private readonly BufferUsageHint m_usageHint;
-        private int m_VBOid = 0;
-        private int m_numVertices = 0;
+    public interface ISSAttributeBuffer
+    {
+        void ConfigureAttribute (int attrLoc);
+    }
+
+    public abstract class SSArrayBuffer<V> where V: struct
+    {
         // dummy vertex for calling bindGLAttributes()
-        private static readonly VB c_dummy = new VB();
-        private static readonly int c_vertexSz = Marshal.SizeOf(c_dummy);
+        protected static readonly V c_dummyElement = new V();
+        protected static readonly int c_elementSz = Marshal.SizeOf(c_dummyElement);
 
-        public int NumVertices { get { return m_numVertices; } }
+        private readonly BufferUsageHint m_usageHint;
+        private int m_bufferIdx = 0;
+        private int m_numElements = 0;
 
-        public SSVertexBuffer(BufferUsageHint hint = BufferUsageHint.DynamicDraw) {
+        public int NumElements { get { return m_numElements; } }
+
+        public SSArrayBuffer(BufferUsageHint hint = BufferUsageHint.DynamicDraw) {
             m_usageHint = hint;
         }
-        
-        public SSVertexBuffer (VB[] vertices,
-                               BufferUsageHint hint = BufferUsageHint.StaticDraw) 
-        : this(hint) {
-            UpdateBufferData(vertices);
-		}
 
-        public void Delete() {
-            GL.DeleteBuffer(m_VBOid);
-            m_VBOid = 0;
-            m_numVertices = 0;
+        public SSArrayBuffer (V[] vertices, 
+                                 BufferUsageHint hint = BufferUsageHint.StaticDraw) 
+            : this(hint) {
+            UpdateBufferData(vertices);
         }
 
-        public void UpdateBufferData(VB[] vertices)
+        public void Delete() {
+            GL.DeleteBuffer(m_bufferIdx);
+            m_bufferIdx = 0;
+            m_numElements = 0;
+        }
+
+        public void UpdateBufferData(V[] vertices)
         {
             genBufferPrivate();
             bindPrivate();
@@ -53,13 +57,51 @@ namespace SimpleScene
             unbindPrivate();
         }
 
+        protected void bindPrivate() {
+            // bind for use
+            GL.BindBuffer(BufferTarget.ArrayBuffer, m_bufferIdx);
+        }
+
+        protected void unbindPrivate() {
+            // unbind from use
+            GL.BindBuffer (BufferTarget.ArrayBuffer, 0);
+        }
+
+        protected void genBufferPrivate() {
+            if (m_bufferIdx == 0) {
+                m_bufferIdx = GL.GenBuffer();
+            }
+        }
+
+        protected void updatePrivate(V[] vertices) {
+            m_numElements = vertices.Length;
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                (IntPtr)(m_numElements * c_elementSz),
+                vertices,
+                m_usageHint);
+        }
+    }
+
+    // http://www.opentk.com/doc/graphics/geometry/vertex-buffer-objects
+    public class SSVertexBuffer<V> : SSArrayBuffer<V>, ISSVertexBuffer
+        where V : struct, ISSVertexLayout 
+    {
+        public SSVertexBuffer(BufferUsageHint hint = BufferUsageHint.DynamicDraw)
+            : base(hint)
+        { }
+
+        public SSVertexBuffer (V[] vertices, 
+                               BufferUsageHint hint = BufferUsageHint.StaticDraw) 
+            : base(vertices, hint)
+        { }
+
         public void DrawArrays(PrimitiveType primType, bool doBind = true) {
             if (doBind) DrawBind();
             drawPrivate(primType);
             if (doBind) DrawUnbind();
         }
 
-        public void UpdateAndDrawArrays(VB[] vertices,
+        public void UpdateAndDrawArrays(V[] vertices,
                                         PrimitiveType primType,
                                         bool doBind = true)
         {
@@ -74,7 +116,7 @@ namespace SimpleScene
             // bind for use and setup for drawing
             bindPrivate();
             GL.PushClientAttrib(ClientAttribMask.ClientAllAttribBits);
-            c_dummy.bindGLAttributes();
+            c_dummyElement.bindGLAttributes();
         }
 
         public void DrawUnbind() {
@@ -83,32 +125,9 @@ namespace SimpleScene
             unbindPrivate();
         }
 
-        private void genBufferPrivate() {
-            if (m_VBOid == 0) {
-                m_VBOid = GL.GenBuffer();
-            }
+        protected void drawPrivate(PrimitiveType primType) {
+            GL.DrawArrays(primType, 0, NumElements);
         }
-
-        private void updatePrivate(VB[] vertices) {
-            m_numVertices = vertices.Length;
-            GL.BufferData(BufferTarget.ArrayBuffer,
-                (IntPtr)(m_numVertices * c_vertexSz),
-               vertices,
-               m_usageHint);
-        }
-
-        private void drawPrivate(PrimitiveType primType) {
-            GL.DrawArrays(primType, 0, m_numVertices);
-        }
-
-		private void bindPrivate() {
-            // bind for use
-			GL.BindBuffer (BufferTarget.ArrayBuffer, m_VBOid);
-		}
-		private void unbindPrivate() {
-            // unbind from use
-			GL.BindBuffer (BufferTarget.ArrayBuffer, 0);
-		}
 	}
 }
 
