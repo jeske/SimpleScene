9cd7ecd29851e6bc1728bd9342e065c5387f8e84
Namone <al2xnord@yahoo.com>
2015-02-04 18:58:23 -0700

    Some more changes

--- Example2DTileGame/SSLineObject.cs
+++ Example2DTileGame/SSLineObject.cs
@@ -18,6 +18,8 @@ namespace Example2DTileGame
         Vector3[,] mapArray = new Vector3[10, 10]; // W x D map (X & Z)
         bool isGenerating = true;
         float x;
+
+        ArrayList vectorList = new ArrayList();
         
         // Default values of square - should never actually set anything to anything
         Vector3 p0 = new Vector3(0, 0, 0);
@@ -29,35 +31,10 @@ namespace Example2DTileGame
         /// <summary>
         /// draw a 'wire - frame' of the map
         /// </summary>
-        public void drawWireFrame(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 middle)
+        public void drawWireFrame(Vector3 point)
         {
-
             // Set points of line
-            GL.Vertex3(p0);
-            GL.Vertex3(p1);
-
-            GL.Vertex3(p3);
-            GL.Vertex3(p1);
-
-            GL.Vertex3(p0);
-            GL.Vertex3(p2);
-
-            GL.Vertex3(p2);
-            GL.Vertex3(p3); 
-
-            // Setting up middle
-
-            GL.Vertex3(p0);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p1);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p2);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p3);
-            GL.Vertex3(middle);
+            GL.Vertex3(point); // alternated to different point every time it is run
         }
 
         /// <summary>
@@ -65,77 +42,69 @@ namespace Example2DTileGame
         /// </summary>
         public override void Render(ref SSRenderConfig renderConfig)
         {
- 
+                
                 base.Render(ref renderConfig);
 
                 //!important!
                 // mode setup
                 SSShaderProgram.DeactivateAll(); // disable GLSL
                 GL.Disable(EnableCap.Texture2D);
-                GL.Disable(EnableCap.Blend);
+                
                 GL.Disable(EnableCap.Lighting);
-
                 //!important!
 
-                float Middle = squareWidth / 2; // Middle point of the square
-
-
+                
                 GL.Begin(PrimitiveType.Lines);
                 GL.Color3(1f, 0f, 0.8f);
-                Random rand = new Random();
+             
 
+               // May only need to set one vector and just change it?
+               for (int i = 0; i < 500; i++)
+               {                   
+                   drawWireFrame((Vector3)vectorList[i]);
+               }                          
+                
 
-                for (int i = 0; i < mapArray.GetLength(0); i++)
-                {
-                    for (int j = 0; j < mapArray.GetLength(1); j++)
-                    {
-
-                        float squareCX = i * squareWidth;
-                        float squareCY = j * squareWidth;
-                        
-                        p0 = new Vector3(squareCX, 0, squareCY);
-                        p1 = new Vector3(squareCX + squareWidth, 0, squareCY);
-                        p2 = new Vector3(squareCX, 0, squareCY + squareWidth);
-                        p3 = new Vector3(squareCX + squareWidth, 0, squareCY + squareWidth);
-
-                        // Determines height
-                        middle = new Vector3(squareCX + Middle, i, squareCY + Middle);
-
-                        mapArray[i, j] = middle;
-                        middle = mapArray[i, j];
-
-                        drawWireFrame(p0, p1, p2, p3, middle); // Draw the wire-frame
-                        
-
-                        /*// If height is 'x' then adjuste other points to match
-                        if (mapArray[i, j].Equals
-                            (new Vector3(squareCX + Middle, rand.Next(), squareCY + Middle)))
-                        {
-                            p0 = new Vector3(squareCX, 0, squareCY);
-                            p1 = new Vector3(squareCX + squareWidth, 0, squareCY);
-                            p2 = new Vector3(squareCX, 0, squareCY + squareWidth);
-                            p3 = new Vector3(squareCX + squareWidth, 0, squareCY + squareWidth);
-                        }
-
-                        Console.WriteLine("Middle: " + mapArray[i, j].ToString());*/
-
-                    }
-                   
-                }
-
-
-                isGenerating = false; // No longer generating
                 GL.End();
-
-            }
+        }
             
             
         
 
         public SSLineObject (Vector3 mapPos) : base()
         {
-            mapPosition = mapPos; // get the map objects position
-            // This runs the base (SSObject) which runs Render (...) which runs drawWireFrame(...)
+           
+            Console.WriteLine("Set points");
+            GL.Begin(PrimitiveType.Lines);
+            
+            for (int i = 0; i < mapArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapArray.GetLength(1); j++)
+                {
+                    float Middle = squareWidth / 2; // Middle point of the square
+                    float squareCX = i * squareWidth;
+                    float squareCY = j * squareWidth;
+
+                    p0 = new Vector3(squareCX, 0, squareCY);
+                    p1 = new Vector3(squareCX + squareWidth, 0, squareCY);
+                    p2 = new Vector3(squareCX, 0, squareCY + squareWidth);
+                    p3 = new Vector3(squareCX + squareWidth, 0, squareCY + squareWidth);
+
+                    // Determines height
+                    middle = new Vector3(squareCX + Middle, i, squareCY + Middle);
+
+                    vectorList.Add(p0); // 0, 5, etc
+                    vectorList.Add(p1); // 1, 6, etc
+                    vectorList.Add(p2); // 2, 7, etc
+                    vectorList.Add(p3); // 3, 8, etc
+                    vectorList.Add(middle); // 4, 9, etc
+
+                     // Draw the wire-frame
+
+                }
+
+            }
+            
         }
     }
 
