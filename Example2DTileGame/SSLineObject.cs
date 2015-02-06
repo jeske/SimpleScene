3bff2d510e231073347396317607c1c213943af4
Namone <al2xnord@yahoo.com>
2015-02-06 11:55:18 -0700

    Separating ArrayList.Add from GL code

--- Example2DTileGame/SSLineObject.cs
+++ Example2DTileGame/SSLineObject.cs
@@ -12,12 +12,17 @@ namespace Example2DTileGame
 {
     public class SSLineObject : SSObject
     {
-
+        #region Variables
         Vector3 mapPosition;
         float squareWidth = 4;
+
         Vector3[,] mapArray = new Vector3[10, 10]; // W x D map (X & Z)
+
         bool isGenerating = true;
         int x = 0;
+
+        float vHeight = 0;
+        float currentHeight;
 
         ArrayList vectorList = new ArrayList();
         
@@ -27,37 +32,20 @@ namespace Example2DTileGame
         Vector3 p2 = new Vector3(0, 0, 0);
         Vector3 p3 = new Vector3(0, 0, 0);
         Vector3 middle = new Vector3(0, 0, 0);
-
+        #endregion
+        
         /// <summary>
         /// draw a 'wire - frame' of the map
         /// </summary>
-        public void drawWireFrame(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 middle)
+        public void drawWireFrame()
         {
-            // Set points of line
-            GL.Vertex3(p0); // alternated to different point every time it is run
-            GL.Vertex3(p1);
-
-            GL.Vertex3(p0);
-            GL.Vertex3(p2);
-
-            GL.Vertex3(p2);
-            GL.Vertex3(p3);
-
-            GL.Vertex3(p3);
-            GL.Vertex3(p1);
-
-            ///////////////Middle
-            GL.Vertex3(p0);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p1);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p2);
-            GL.Vertex3(middle);
-
-            GL.Vertex3(p3);
-            GL.Vertex3(middle);
+            GL.Begin(PrimitiveType.Lines);
+            foreach (Vector3 v in vectorList)
+            {
+                // Draw each point added
+                GL.Vertex3(v);
+            }
+            GL.End();
         }
 
         /// <summary>
@@ -71,63 +59,61 @@ namespace Example2DTileGame
                 //!important!
                 // mode setup
                 SSShaderProgram.DeactivateAll(); // disable GLSL
-                GL.Disable(EnableCap.Texture2D);
-                
+                GL.Disable(EnableCap.Texture2D);                
                 GL.Disable(EnableCap.Lighting);
-                //!important!
-
+                //!important!                
                 
-                GL.Begin(PrimitiveType.Lines);
-                GL.Color3(1f, 0f, 0.8f);
-
-                Console.WriteLine("Set points");
-                GL.Begin(PrimitiveType.Lines);
-
-                for (int i = 0; i < mapArray.GetLength(0); i++)
-                {
-                    for (int j = 0; j < mapArray.GetLength(1); j++)
-                    {
-                        float Middle = squareWidth / 2; // Middle point of the square
-                        float squareCX = i * squareWidth;
-                        float squareCY = j * squareWidth;
-
-                        p0 = new Vector3(squareCX, 0, squareCY);
-                        p1 = new Vector3(squareCX + squareWidth, 0, squareCY);
-                        p2 = new Vector3(squareCX, 0, squareCY + squareWidth);
-                        p3 = new Vector3(squareCX + squareWidth, 0, squareCY + squareWidth);
-
-                        // Determines height
-                        middle = new Vector3(squareCX + Middle, i, squareCY + Middle);
-
-                        vectorList.Add(p0); // 0, 5, etc
-                        vectorList.Add(p1); // 1, 6, etc
-                        vectorList.Add(p2); // 2, 7, etc
-                        vectorList.Add(p3); // 3, 8, etc
-                        vectorList.Add(middle); // 4, 9, etc
-
-                        // Draw the wire-frame
-                        
-                        drawWireFrame((Vector3)vectorList[x], (Vector3)vectorList[x + 1], (Vector3)vectorList[x + 2], 
-                            (Vector3)vectorList[x + 3], (Vector3)vectorList[x + 4]);
-
-                        // Helps us accurately move through array-list
-                        x += 5;
-                    }
-
-                }
-
-                GL.End();
+                GL.Color3(1f, 0.1f, 0.8f);
+                drawWireFrame(); // Draw it               
         }
-            
-            
-        
+
+        /// <summary>
+        /// Adds points into array-list
+        /// </summary>
+        public void addToArray(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 Middle)
+        {
+            vectorList.Add(p0); vectorList.Add(p1);
+            vectorList.Add(p0); vectorList.Add(p2);
+            vectorList.Add(p2); vectorList.Add(p3);
+            vectorList.Add(p3); vectorList.Add(p1);
+            vectorList.Add(p0); vectorList.Add(Middle);
+            vectorList.Add(p1); vectorList.Add(Middle);
+            vectorList.Add(p2); vectorList.Add(Middle);
+            vectorList.Add(p3); vectorList.Add(Middle);
+        }
 
         public SSLineObject (Vector3 mapPos) : base()
         {
-           
-            
+            Random rand = new Random();
+            Console.WriteLine("Set points");
+            for (int i = 0; i < mapArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapArray.GetLength(1); j++)
+                {
+                    float Middle = squareWidth / 2; // Middle point of the square
+                    float squareCX = i * squareWidth;
+                    float squareCY = j * squareWidth;
+
+                    p0 = new Vector3(squareCX, vHeight, squareCY);
+                    p1 = new Vector3(squareCX + squareWidth, vHeight, squareCY);
+                    p2 = new Vector3(squareCX, vHeight, squareCY + squareWidth);
+                    p3 = new Vector3(squareCX + squareWidth, vHeight, squareCY + squareWidth);
+
+                    currentHeight = rand.Next(0, 10);
+                    // Determines height
+                    middle = new Vector3(squareCX + Middle, currentHeight, squareCY + Middle);
+
+                    
+
+                    addToArray(p0, p1, p2, p3, middle);
+
+                }
+
+            }
             
         }
+
+
     }
 
 }
