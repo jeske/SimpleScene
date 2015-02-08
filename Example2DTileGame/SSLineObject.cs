c2668482013597ebcbf4988535b6f54eb6b59dc1
Namone <al2xnord@yahoo.com>
2015-02-07 21:14:01 -0700

    Struct work

--- Example2DTileGame/SSLineObject.cs
+++ Example2DTileGame/SSLineObject.cs
@@ -16,7 +16,7 @@ namespace Example2DTileGame
         Vector3 mapPosition;
         float squareWidth = 4;
 
-        Vector3[,] mapArray = new Vector3[10, 10]; // W x D map (X & Z)
+        Vector3[,] mapArray = new Vector3[50, 50]; // W x D map (X & Z)
 
         bool isGenerating = true;
         int x = 0;
@@ -32,20 +32,32 @@ namespace Example2DTileGame
         Vector3 p2 = new Vector3(0, 0, 0);
         Vector3 p3 = new Vector3(0, 0, 0);
         Vector3 middle = new Vector3(0, 0, 0);
+
+
+        public struct LineData
+        {
+            public Vector3 Pos;
+        }
+
+        LineData[,] data;
         #endregion
         
         /// <summary>
         /// draw a 'wire - frame' of the map
         /// </summary>
-        public void drawWireFrame()
+        public void drawWireFrame(LineData[,] data)
         {
             GL.Begin(PrimitiveType.Lines);
-            foreach (Vector3 v in vectorList)
+
+            for (int i = 0; i < mapArray.GetLength(0); i++)
             {
-                // Draw each point added
-                GL.Vertex3(v);
+                for (int j = 0; j < mapArray.GetLength(1); j++)
+                {
+                    GL.Vertex3(data[i, j].Pos);
+                }
             }
-            GL.End();
+
+                GL.End();
         }
 
         /// <summary>
@@ -62,16 +74,16 @@ namespace Example2DTileGame
                 GL.Disable(EnableCap.Texture2D);                
                 GL.Disable(EnableCap.Lighting);
                 //!important!                
-                
-                GL.Color3(1f, 0.1f, 0.8f);
-                drawWireFrame(); // Draw it               
+                GL.Color3(1f, 1f, 1f);
+                drawWireFrame(data); // Draw it               
         }
 
         /// <summary>
         /// Adds points into array-list
         /// </summary>
-        public void addToArray(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 Middle)
-        {
+        public void AddToArray(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, Vector3 Middle)
+        {            
+
             vectorList.Add(p0); vectorList.Add(p1);
             vectorList.Add(p0); vectorList.Add(p2);
             vectorList.Add(p2); vectorList.Add(p3);
@@ -82,10 +94,20 @@ namespace Example2DTileGame
             vectorList.Add(p3); vectorList.Add(Middle);
         }
 
+        /// <summary>
+        /// Relax the map
+        /// </summary>
+        public void Smoothing()
+        {
+
+        }
+
         public SSLineObject (Vector3 mapPos) : base()
         {
+            data = new LineData[mapArray.GetLength(0), mapArray.GetLength(1)];
             Random rand = new Random();
             Console.WriteLine("Set points");
+     
             for (int i = 0; i < mapArray.GetLength(0); i++)
             {
                 for (int j = 0; j < mapArray.GetLength(1); j++)
@@ -99,14 +121,13 @@ namespace Example2DTileGame
                     p2 = new Vector3(squareCX, vHeight, squareCY + squareWidth);
                     p3 = new Vector3(squareCX + squareWidth, vHeight, squareCY + squareWidth);
 
-                    currentHeight = rand.Next(0, 10);
                     // Determines height
-                    middle = new Vector3(squareCX + Middle, currentHeight, squareCY + Middle);
+                    middle = new Vector3(squareCX + Middle, rand.Next(0, 10), squareCY + Middle);
 
-                    
+                    data[i, j].Pos = p0;                    
 
-                    addToArray(p0, p1, p2, p3, middle);
-
+                    AddToArray(p0, p1, p2, p3, middle);
+     
                 }
 
             }
