1d4dadc0d0ea009812e4b6a1cf6f5d69ba225d5f
HoneyHazard <zeromemory@gmail.com>
2015-04-16 00:42:29 -0700

    Trying to use normals

--- SimpleScene/Meshes/Skeletal/SSSkeletalRenderMesh.cs
+++ SimpleScene/Meshes/Skeletal/SSSkeletalRenderMesh.cs
@@ -3,16 +3,16 @@ using OpenTK;
 
 namespace SimpleScene
 {
-	public class SSSkeletalIIndexedMesh : SSIndexedMesh<SSVertex_PosTex>
+	public class SSSkeletalIIndexedMesh : SSIndexedMesh<SSVertex_PosNormTex>
 	{
 		protected readonly SSSkeletalMeshMD5 m_md5;
-		protected readonly SSVertex_PosTex[] m_vertices;
+		protected readonly SSVertex_PosNormTex[] m_vertices;
 
 		public SSSkeletalIIndexedMesh (SSSkeletalMeshMD5 md5)
 			: base(null, md5.Indices)
 		{
 			m_md5 = md5;
-			m_vertices = new SSVertex_PosTex[md5.NumVertices];
+			m_vertices = new SSVertex_PosNormTex[md5.NumVertices];
 			for (int v = 0; v < md5.NumVertices; ++v) {
 				m_vertices [v].TexCoord = md5.TextureCoords (v);
 			}
@@ -25,7 +25,7 @@ namespace SimpleScene
 		{
 			for (int v = 0; v < m_md5.NumVertices; ++v) {
 				m_vertices [v].Position = m_md5.ComputeVertexPos (v);
-				// TODO Normals
+				m_vertices [v].Normal = m_md5.ComputeVertexNormal (v);
 			}
 			m_vbo.UpdateBufferData (m_vertices);
 		}
