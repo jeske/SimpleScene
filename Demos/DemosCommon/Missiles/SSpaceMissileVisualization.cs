44b8b4ca2831425129f5c0289e1c4c7e2be3cd9f
HoneyHazard <zeromemory@gmail.com>
2015-10-03 17:28:22 -0700

    More drafting of the missile classes

---
+++ Demos/DemosCommon/Missiles/SSpaceMissileVisualization.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using OpenTK;
+
+namespace SimpleScene.Demos
+{
+    public interface ISSpaceMissileEjectionDriver
+    {
+        void init(SSpaceMissileVisualizion missile, Vector3 clusterPos, Vector3 clusterVel,
+                  ref Vector3 pos, ref Vector3 dir, ref Vector3 velocity, ref Vector3 angularVelocity);
+        void update(SSpaceMissileVisualizion missile, float timeElapsed, 
+                    ref float thrust, ref Vector3 lateralAcc);
+    }
+
+    public interface ISSpaceMissilePursuitDriver
+    {
+        void update(SSpaceMissileVisualizion missile, float timeElapsed, 
+            ref float thrust, ref Vector3 lateralAcc);
+    }
+
+    public class SSpaceMissileVisualizion
+    {
+        // TODO actual rendering constructs. Where do they go?
+
+        public enum State { Ejection, Pursuit, Intercepted };
+
+        public SSpaceMissileVisualizerCluster cluster { get { return _cluster; } }
+        public int clusterId { get { return _clusterId; } }
+
+        public Vector3 position { get { return _position; } }
+        public Vector3 velocity { get { return _velocity; } }
+        public Vector3 angularVelocity { get { return _angularVelocity; } }
+        public Vector3 acceleration { get { return _acceleration; } }
+        public Vector3 direction { get { return _direction; } }
+        public Vector3 lateralAcceleration { get { return _lateralAcceleration; } }
+        public float thrust { get { return _thrust; } }
+
+        public Vector3 prevTargetDir { get { return _prevTargetDir; } }
+
+        /// <summary>
+        /// The cluster this missile belongs to. Variable gives access to params, target, other missiles.
+        /// </summary>
+        protected readonly SSpaceMissileVisualizerCluster _cluster; 
+
+        /// <summary>
+        /// ID within a cluster. Can be referenced by ejection/pursuit behaviors.
+        /// </summary>
+        protected readonly int _clusterId;
+
+        /// <summary>
+        /// High level state of what the missile is doing
+        /// </summary>
+        protected State _state = State.Ejection;
+
+        /// <summary>
+        /// Time before before the target must be hit. Can get updated from an external simulation.
+        /// </summary>
+        protected float _timeToHit = 0f;
+
+        protected Vector3 _position = Vector3.Zero;
+        protected Vector3 _velocity = Vector3.Zero;
+        protected Vector3 _angularVelocity = Vector3.Zero;
+        protected Vector3 _acceleration = Vector3.Zero;
+        protected Vector3 _lateralAcceleration = Vector3.Zero;
+        protected Vector3 _direction = Vector3.Zero;
+        protected float _thrust = 0f;
+
+        protected float _timeElapsed = 0f;
+        protected Vector3 _prevTargetDir;
+
+        public SSpaceMissileVisualizion(SSpaceMissileVisualizerCluster cluster, int clusterId,
+                                        Vector3 initClusterPos, Vector3 initClusterVel, float timeToHit)
+        {
+            _cluster = cluster;
+            _clusterId = clusterId;
+            _timeToHit = timeToHit;
+
+            var ejection = _cluster.parameters.ejectionDriver;
+            ejection.init(this, initClusterPos, initClusterVel,
+                ref _position, ref _direction, ref _velocity, ref _angularVelocity);
+
+            _prevTargetDir = (cluster.target.position - _position).Normalized();
+            ejection.update(this, 0f, ref _thrust, ref _lateralAcceleration);
+        }
+
+        public void updateExecution(float timeElapsed)
+        {
+            _timeElapsed = timeElapsed;
+            Vector3 toTarget = cluster.target.position - _position;
+            Vector3 targetDir = toTarget.Normalized();
+
+            var mParams = _cluster.parameters;
+            switch (_state) {
+            case State.Ejection:
+                mParams.ejectionDriver.update(this, timeElapsed, ref _thrust, ref _lateralAcceleration);
+                break;
+            case State.Pursuit:
+                mParams.pursuitDriver.update(this, timeElapsed, ref _thrust, ref _lateralAcceleration);
+                break;
+            case State.Intercepted:
+                // todo
+                break;
+            }
+
+            _prevTargetDir = targetDir;
+        }
+
+        public void updateTimeToHit(float timeToHit)
+        {
+            _timeToHit = timeToHit;
+            // TODO adjust things (thrust?) so that distanceToTarget = closing velocity * timeToHit
+        }
+
+        public void updateRenderingObjects(float timeElapsed)
+        {
+
+        }
+    }
+
+   
+}
+
---
+++ Demos/DemosCommon/Missiles/SSpaceMissilesVisuals.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+
+namespace SimpleScene.Demos
+{
+    /// <summary>
+    /// Missile target provides target position, velocity, and acceleration
+    /// </summary>
+    public interface ISSpaceMissileTarget
+    {
+        Vector3 position { get; }
+        Vector3 velocity { get; }
+        Vector3 acceleration { get; } // make optional?
+        bool isAlive{ get; } // target is structurally intact
+    };
+
+    public class SSpaceMissilesVisuals
+    {
+        protected List<SSpaceMissileVisualizerCluster> _clusters
+            = new List<SSpaceMissileVisualizerCluster>();
+
+        public void launchCluster(
+            Vector3 launchPos, Vector3 launchVel, 
+            ISSpaceMissileTarget target, float timeToHit,
+            SSpaceMissileVisualizerParameters clusterParams)
+        {
+
+        }
+
+        public void updateVisualization(float timeElapsed)
+        {
+            foreach (var cluster in _clusters) {
+                cluster.updateVisualization(timeElapsed);
+            }
+        }
+
+        /// <summary>
+        /// Terminate a missile in a cluster before it reaches its target
+        /// </summary>
+        public void interceptMissile(SSpaceMissileVisualizion missile, float delay)
+        {
+            // TODO
+        }
+
+
+    }
+
+    public class SSpaceMissileVisualizerCluster
+    {
+        protected readonly SSpaceMissileVisualizion[] _missiles;
+        protected readonly ISSpaceMissileTarget _target;
+        protected readonly SSpaceMissileVisualizerParameters _parameters;
+
+        public SSpaceMissileVisualizerParameters parameters { get { return _parameters; } }
+        public ISSpaceMissileTarget target { get { return _target; } }
+
+        SSpaceMissileVisualizerCluster(
+            Vector3 initPos, Vector3 initVel, int numMissiles,
+            ISSpaceMissileTarget target, float timeToHit,
+            SSpaceMissileVisualizerParameters mParams)
+        {
+            _target = target;
+            _parameters = mParams;
+            _missiles = new SSpaceMissileVisualizion[numMissiles];
+            for (int i = 0; i < numMissiles; ++i) {
+                _missiles [i] = new SSpaceMissileVisualizion (this, i, initPos, initVel, timeToHit);
+            }
+        }
+
+        public void updateVisualization(float timeElapsed)
+        {
+            foreach (var missile in _missiles) {
+                missile.updateExecution(timeElapsed);
+            }
+        }
+
+        public void updateTimeToHit(float timeToHit)
+        {
+            foreach (var missile in _missiles) {
+                missile.updateTimeToHit(timeToHit);
+            }
+        }
+    }
+
+    public class SSpaceMissileVisualizerParameters
+    {
+        public float minActivationTime = 1f;
+
+        public ISSpaceMissileEjectionDriver ejectionDriver; // TODO = ?
+        public ISSpaceMissileEjectionDriver pursuitDriver; // TODO =
+
+        // TODO: max lateral acceleration?
+        // TODO throw/programmed maneuver pattern
+        // TODO: different homing behavior types?
+        // TODO: fuel strategy?
+    }
+}
+
